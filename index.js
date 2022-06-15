f16f91092dbb79f003ee8079197ff69b74ef2595
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-15 22:23:19 +0300

    commit tree 592e9682b62f32585f00257257e6b3cd49ca40f1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const myFirst = document.querySelector('main');
+myFirst.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Rosemary is the champion";
